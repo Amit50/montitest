df947d8ec23d964d75d784ccd5f9f30f6c021b37
Amit50 <bugalia.amit@gmail.com>
2019-11-25 23:16:21 +0530

    Add files via upload

---
+++ Basics/src/Tibrewal/program3.java
@@ -0,0 +1,14 @@
+package Tibrewal;
+
+public class program3 {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		System.out.println("Enter two numbers to calculate their sum");
+
+		c=a+b;
+		System.out.println("Sum of the integerz ="+c);
+
+	}
+
+}
